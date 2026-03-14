dfeb13e2385158aed15416ebc42939000ee9f09f
Vishwajit Gaikwad <vishwajitgaikwad1123@gmail.com>
2026-03-14 13:29:34 +0530

    Add diamond pattern printing functions in JavaScript
    print diamond pattern

---
+++ JavascriptTest0001.js
@@ -0,0 +1,30 @@
+function printPatternSpaces(count) {
+    let str = "";
+    for (let i = 0; i < count; i++) {
+        str += " ";
+    }
+    return str;
+}
+
+function printPatternRow(row, n) {
+    let line = printPatternSpaces(n - row);
+
+    for (let j = 1; j <= 2 * row - 1; j++) {
+        if (j === 1 || j === 2 * row - 1) {
+            line += "*";
+        } else {
+            line += " ";
+        }
+    }
+
+    console.log(line);
+}
+
+function printDiamondPattern(n) {
+    for (let i = 1; i <= 2 * n - 1; i++) {
+        let row = (i <= n) ? i : 2 * n - i;
+        printPatternRow(row, n);
+    }
+}
+
+printDiamondPattern(5);
